2f2634d0a6264a9a334978983189fd841904f5f4
lizhongxiang <lzx@qq.com>
2022-12-21 11:33:06 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## 通过ssh通道映射数据库到本地端口
+## 详细配置查看 resources/SSH.properties
---
+++ src/main/java/org/myjssh/Main.java
@@ -0,0 +1,10 @@
+package org.myjssh;
+
+import org.myjssh.service.SSHService;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        SSHService.sshRun();
+    }
+}
